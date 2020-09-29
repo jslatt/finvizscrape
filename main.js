0c6d0a1e6242846c091fd75625963e85232a72e5
James <james@blueve.in>
2020-09-28 19:46:10 -0700

    Remove preload.js stuff and remove UI menu.

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function createWindow () {
     }
   })
 
-  //mainWindow.setMenu(null);
+  mainWindow.setMenu(null);
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
